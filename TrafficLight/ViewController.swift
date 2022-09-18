15358f02317bf49b562170e139b7a2b5c1e14c92
Vladislav <vsuslov7@gmail.com>
2022-09-18 23:54:56 +0300

    Created traffic light

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -7,13 +7,57 @@
 
 import UIKit
 
+
+//MARK: EnumOfColors
+enum CurrentColor {
+    case red
+    case yellow
+    case green
+}
+
 class ViewController: UIViewController {
 
+    @IBOutlet var redView: UIView!
+    @IBOutlet var yellowView: UIView!
+    @IBOutlet var greenView: UIView!
+    @IBOutlet var startButton: UIButton!
+    
+    let lightOn: CGFloat = 1.0
+    let lightOff: CGFloat = 0.3
+    var currentColor = CurrentColor.red
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redView.alpha = lightOff
+        yellowView.alpha = lightOff
+        greenView.alpha = lightOff
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        redView.layer.cornerRadius = redView.frame.height / 2
+        yellowView.layer.cornerRadius = yellowView.frame.height / 2
+        greenView.layer.cornerRadius = greenView.frame.height / 2
+        startButton.layer.cornerRadius = 20
     }
 
-
+    @IBAction func changeColor() {
+        startButton.setTitle("NEXT", for: .normal)
+        
+        switch currentColor {
+        case .red:
+            greenView.alpha = lightOff
+            redView.alpha = lightOn
+            currentColor = .yellow
+        case .yellow:
+            redView.alpha = lightOff
+            yellowView.alpha = lightOn
+            currentColor = .green
+        case .green:
+            yellowView.alpha = lightOff
+            greenView.alpha = lightOn
+            currentColor = .red
+        }
+    }
 }
 
